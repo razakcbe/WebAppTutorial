f3c66a385a6e5e2c879364fc68da935bf2baca3f
Abdul Razak <razakcbe@gmail.com>
2017-06-19 12:52:55 +0530

    Changed repo file and deleted controller

--- src/main/java/com/razak/springboot/webapptutorial/repo/JsonDataRepository.java
+++ src/main/java/com/razak/springboot/webapptutorial/repo/JsonDataRepository.java
@@ -1,11 +1,28 @@
 package com.razak.springboot.webapptutorial.repo;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.razak.springboot.webapptutorial.entity.JsonData;
 
-@RepositoryRestResource
+@RepositoryRestResource(path = "data")
 public interface JsonDataRepository extends PagingAndSortingRepository<JsonData, Long> {
-
+	
+	/**
+	 * http://localhost:8080/data POST
+	 * {"name":"rasik", "data":"testdata"}
+	 */
+	
+	/**
+	 * http://localhost:8080/data GET
+	 */
+	
+	/**
+	 * http://localhost:8080/data/search/findByName?name=razak
+	 */
+	
+	List<JsonData> findByName(@Param("name") String name);
 }
